771bff67c605f8ab26d1ed01f97e4ce7c04e18b7
Irina263 <72856015+Irina263@users.noreply.github.com>
2022-03-16 14:57:47 +0300

    Add files via upload

---
+++ сайт/index.php
@@ -0,0 +1,93 @@
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name= "description" content="Дары Пензы - Уникальные продукты от лучших производителей">
+    <meta name= "keywords" content= "Ферма, Фермеры России, Купить семена, Агропродукты">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <title>Дары пензы</title>
+</head>
+<body>
+<script src="/js/jquery-3.2.1.min"></script>
+    <header class="header">
+        <a href="#" class="logo">
+            <img src="/img/logo.png" alt="logo">
+        </a>
+        <nav>
+            <ul class="nav">
+                <li class="nav-item">  
+                    <a href="/basket.php" class="nav-link--store">Корзина</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/register.php" class="nav-link--profile">Профиль</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <section>
+        <div class="line">
+            <img class ="line" src="/img/linemenu.png" class="main-img_image">
+        </div>
+    </selection>
+    <div class="wrapper">
+        <div class="grap">
+    <main class="block">
+   <div class="main">
+        <div class="main-text">
+            <h1 class="main-text-title">
+                 Дары пензы
+            </h1>
+            <p class="main-text-dar"><span>Свежие овощи и продукты<br>
+                    из лучшего сельхозяйства<br>
+                    города Пензы </span>
+            </p>
+            <a href="/register.php" class="nav-link--profile">Каталог</a>
+               </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+    </section>
+    <main class="maincataloge">
+    <div class="cataloge">
+        <div class="shop">
+            <h1>Каталог</h1>
+            <p class="cataloge-text" style="height: 15px;">
+                        <span>6 товаров</span>
+             </p>
+         </p>
+    </div>
+    </div>
+</main>
+	<!-- Блок товаров -->
+    <?php include "/openserver/domains/localhost/Store/store.php"?> 
+    <!-- Блок товаров -->
+   	<!-- Блок about -->
+       <?php include "/openserver/domains/localhost/Store/about.php"?> 
+    <!-- Блок about -->
+    <div class="action" onclick="actionToggle();">
+        <span>+</span>
+        <ul>
+          <li><img src="https://img.icons8.com/ios-glyphs/30/000000/vk-circled.png" alt="" width="25px">Перейти в  VK</li>
+          <li><img src="https://img.icons8.com/material-outlined/24/000000/instagram-new--v2.png" alt="" width="25px">Перейти в Instagram</li>
+        </ul>
+                                         </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="/js/main.js"></script>
+<script src="/js/script.js"></script>
+    </body>
+</html>
